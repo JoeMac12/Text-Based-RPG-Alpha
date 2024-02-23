c36a645216f2e955dfc2e7c29980cbe9fed2782e
Arman <144450534+JoeMac12@users.noreply.github.com>
2024-02-22 21:12:58 -0400

    create enemy base class

---
+++ Text Based RPG Alpha/Classes/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Based_RPG_Alpha.Classes
+{
+    internal abstract class Enemy
+    {
+        public int HP { get; protected set; } // HP
+        public int AttackDamage { get; protected set; } // Damage
+        public (int x, int y) Position { get; protected set; } // Start area
+
+        protected Enemy(int hp, int attackDamage, int startX, int startY) // Initialize enemy with it's custom stats
+        {
+            HP = hp;
+            AttackDamage = attackDamage;
+            Position = (startX, startY);
+        }
+
+        public void TakeDamage(int damage) // Take Damage from player
+        {
+            HP -= damage;
+        }
+
+        public abstract void Move(Map map); // Base class for enemy movement
+
+        // Might add other methods here later idk
+    }
+}
